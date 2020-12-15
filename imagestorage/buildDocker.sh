5f4bc8974f935d9383c4d6ee4cd848861160d22b
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2020-12-15 18:15:07 -0500

    Updates to use mvnw for executing mvn tasks

--- imagestorage/buildDocker.sh
+++ imagestorage/buildDocker.sh
@@ -3,11 +3,11 @@
 #Build Image Storage
 
 #JVM Image
-#mvn package
+#mvnw package
 #docker build -f src/main/docker/Dockerfile.jvm.alpine -t malcolmpereira/imagestorage-quarkus-jvm .
 
 #Native Image
 #Make sure docker has alteast 6GB of memory allocated
-mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.build=true
+mvnw package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.build=true
 docker build -f src/main/docker/Dockerfile.native -t malcolmpereira/imagestorage-quarkus-native .
 cd ..
--- imagevalidation/buildDocker.sh
+++ imagevalidation/buildDocker.sh
@@ -4,9 +4,9 @@
 #Build Image Validation
 
 #JVM Image
-mvn package
+mvnw package
 docker build -f src/main/docker/Dockerfile.jvm.alpine -t malcolmpereira/imagevalidation-quarkus-jvm .
 
 #Native Image Not Possible At this time since GRAAL does not support java.awt Graphics/BufferedImage
-#mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.build=true -Dquarkus.native.native-image-xmx=3g
+#mvnw package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.build=true -Dquarkus.native.native-image-xmx=3g
 #docker build -f src/main/docker/Dockerfile.native -t malcolmpereira/imagevalidation-quarkus-native .
